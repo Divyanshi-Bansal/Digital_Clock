41252dcc53697b021738bfe98666565cf7c36075
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-10-13 13:42:06 +0530

    get date

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ let digitalClock = () => {
     let hours = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
+    let date1 = date.getUTCDate();
 
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("min").innerHTML = min;
     document.getElementById("sec").innerHTML = sec;
+    document.getElementById("date").innerHTML = date1;
 }
 
 digitalClock();
