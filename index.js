49165bcc8494ed35d8ea8033f23743b0232e74c2
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-10-13 13:30:30 +0530

    values transfer by id

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ let digitalClock = () => {
     let min = date.getMinutes();
     let sec = date.getSeconds();
 
-    
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("min").innerHTML = min;
+    document.getElementById("sec").innerHTML = sec;
 }
