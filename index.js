c99d10fda6d4bdea300f41cdf8404314d7888ac2
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-10-13 15:45:29 +0530

    change month into words

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ let digitalClock = () => {
     weekday[5] = 'Friday';
     weekday[6] = 'Saturday';
 
+    var monthName = new Array(12);
+    monthName[1] = 'Jan';
+    monthName[2] = 'Feb';
+    monthName[3] = 'March;';
+    monthName[4] = 'April';
+    monthName[5] = 'May';
+    monthName[6] = 'June';
+    monthName[7] = 'July';
+    monthName[8] = 'Aug';
+    monthName[9] = 'Sep';
+    monthName[10] = 'Oct';
+    monthName[11] = 'Nov';
+    monthName[12] = 'Dec';
+
     let not  = hour >=12 ? 'pm' : 'am';
 
     let hours = hour >12 ? hour-12 : hour ;
@@ -26,7 +40,7 @@ let digitalClock = () => {
     document.getElementById("sec").innerHTML = sec;
     document.getElementById('not').innerHTML = not;
     document.getElementById("date").innerHTML = date1;
-    document.getElementById("month").innerHTML = month + 1;
+    document.getElementById("month").innerHTML = monthName[month + 1];
     document.getElementById("year").innerHTML = year;
     document.getElementById("day").innerHTML = weekday[day];
 
